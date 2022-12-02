c57f81d0b79c5ddf80f672a7a9bcbb015f89f5e1
WangZX <asdfwdsfrg@outlook.com>
2022-12-02 16:47:03 +0800

    Delete cuda_module directory

--- cuda_module/include/blend_feature.h
+++
@@ -1,9 +0,0 @@
-void launch_add2(float *c,
-                 const float *nodes,
-                 const float *pts,
-                 const float r,
-                 int n_nodes);
-//given posed nodes & sample_d pts 
-//every nodes has its‘ influence range r
-//first find the number of pts in side nodes_i's field , marked as si
-//then find the maximum s' = max(s1, s2, si)
